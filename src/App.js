727ec4f40eb3215c0a0ffc69e76ebc6c6fb20b6c
Danish Saeed <121003894+mrDanish20@users.noreply.github.com>
2026-01-28 14:56:21 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+
+import Home from './Components/Home';
+import About from './Components/About';
+import Features from './Components/Features';
+import Services from './Components/Services';
+import Exercise from './Components/Exercise';
+import Contactus from './Components/Contactus';
+import Login from './Components/Login';
+
+
+function App() {
+  return (
+    <HashRouter >
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Features' element={<Features />} />
+          <Route path='/Services' element={<Services />} />
+          <Route path='/Exercise' element={<Exercise />} />
+          <Route path='/Contactus' element={<Contactus />} />
+          <Route path='/Login' element={<Login />}/>
+
+
+
+        </Routes>
+
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
